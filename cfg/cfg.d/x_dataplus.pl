e382447cb7cd17f3a34c51ef07ebf807c29a7f6e
Patrick McSweeney <mcsweeney.patrick@gmail.com>
2013-02-11 15:59:57 +0000

    * Added DOI field did some reshuffling in conf and EPMC file.

---
+++ cfg/cfg.d/x_dataplus.pl
@@ -0,0 +1,224 @@
+$c->{plugins}->{"Screen::NewDataset"}->{params}->{disable} = 0;
+
+$c->{uk_data_archive_eprint_render} = $c->{eprint_render};
+
+$c->{eprint_render} = sub
+{
+        my( $eprint, $repository, $preview ) = @_;
+
+	my ($page, $title, $links ) = $repository->call("uk_data_archive_eprint_render", $eprint, $repository, $preview);
+
+	my $div_right = $repository->make_element('div', class => "rd_citation_right");
+	$page->appendChild($div_right);
+
+	my $xml = $repository->xml;
+	my $heading = $xml->create_element('h2', class => "file_list_heading");
+    	$heading->appendChild($xml->create_text_node("Available Files"));
+	$div_right->appendChild($heading);
+	
+	if(scalar $eprint->get_all_documents() eq 0 ){
+		my $nodocs = $repository->make_element('p', class => "file_list_nodocs");
+	        $nodocs->appendChild($repository->make_text(" No Files to display"));
+		$div_right->appendChild($nodocs);
+		return ( $page, $title, $links );
+	}
+	my $chunks = {};
+	foreach my $doc ($eprint->get_all_documents())
+	{
+                $div_right->appendChild($doc->render_citation_link("uk_data_archive_full"));
+#		if(!defined $chunks->{$doc->value("content")."key"})
+#		{
+#			my $doc_frag = $xml->create_document_fragment;
+#			$div_right->appendChild($doc_frag);
+#			$chunks->{$doc->value("content")."key"} = $doc_frag;
+#			my $content_heading = $xml->create_element('h2', class => "file_title");
+#			$content_heading->appendChild($repository->html_phrase("content_typename_".$doc->value("content")));
+#			$doc_frag->appendChild($content_heading);
+#		}
+#
+#		$chunks->{$doc->value("content")."key"}->appendChild($doc->render_citation_link("uk_data_archive_full"));
+	}
+	
+
+
+
+        return( $page, $title, $links );
+};
+
+push (@{$c->{summary_page_metadata}}, qw/
+      alt_title
+      creators
+      corp_creators
+      data_type
+      contributors
+      funders
+      date_coll
+      date_range
+      grant
+      date
+      date_type
+      geog
+      coll_samp
+      bounding_box
+      legal_ethical
+      lineage
+      note
+      language
+      relation
+      projects
+      ispublished
+      publisher
+      restrictions
+      copyright_holders
+      contact_email
+      lastmod
+      /
+);
+
+$c->add_dataset_field( "eprint", {
+        name => 'bounding_box',
+        type => 'compound',
+        fields => [
+                {
+                        sub_name => 'north_edge',
+                        type => 'float',
+                },
+                {
+                        sub_name => 'east_edge',
+                        type => 'float',
+                },
+                {
+                        sub_name => 'south_edge',
+                        type => 'float',
+                },
+                {
+                        sub_name => 'west_edge',
+                        type => 'float',
+                },
+        ],
+}, reuse => 1 );
+
+$c->add_dataset_field( "eprint", {
+        name => 'alt_title',
+        type => 'longtext',
+        input_rows => 3,
+}, reuse => 1 );
+
+
+$c->add_dataset_field( "eprint", {
+        name => 'coll_samp',
+        type => 'longtext',
+        input_rows => '10',
+}, reuse => 1 );
+
+$c->add_dataset_field( "eprint", {
+        name => 'grant',
+        type => 'text',
+}, reuse => 1 );
+
+$c->add_dataset_field( "eprint", {
+        name => 'lineage',
+        type => 'longtext',
+        input_rows => '3',
+
+}, reuse => 1 );
+
+$c->add_dataset_field( "eprint", {
+        name => 'restrictions',
+        type => 'text',
+}, reuse => 1 );
+
+$c->add_dataset_field( "eprint", {
+        name => 'geog',
+        type => 'text',
+}, reuse => 1 );
+
+$c->add_dataset_field( "eprint", {
+        name => 'language',
+        type => 'text',
+
+}, reuse => 1 );
+
+$c->add_dataset_field( "eprint", {       
+	name => 'legal_ethical',
+        type => 'longtext',
+        input_rows => '10',
+}, reuse => 1 );
+
+$c->add_dataset_field( "eprint", {   name => 'terms_conditions_agreement',
+    type => 'boolean',
+    input_style => 'medium',
+}, reuse => 1 );
+
+$c->add_dataset_field( "eprint", {   name => 'date_coll',
+        type => 'compound',
+        fields => [
+                {
+                        sub_name => 'date_from',
+                        type => 'date',
+                    render_res => 'day',
+        },
+                {
+                        sub_name => 'date_to',
+                        type => 'date',
+        },
+        ],
+}, reuse => 1 );
+
+$c->add_dataset_field( "eprint", {
+        name => 'date_range',
+        type => 'compound',
+        fields => [
+                {
+                        sub_name => 'date_from',
+                        type => 'date',
+                    render_res => 'day',
+        },
+                {
+                        sub_name => 'date_to',
+                        type => 'date',
+        },
+        ],
+}, reuse => 1 );
+
+$c->add_dataset_field( "eprint", {
+   name => 'original_publisher',
+   type => 'text',
+}, reuse => 1 );
+
+$c->add_dataset_field( "eprint", {
+        name => 'related_resources',
+        type => 'compound',
+        multiple => 1,
+        render_value => 'EPrints::Extras::render_url_truncate_end',
+        fields => [
+                {
+                        sub_name => 'url',
+                        type => 'url',
+                        input_cols => 40,
+                },
+                {
+                        sub_name => 'type',
+                        type => 'set',
+                        render_quiet => 1,
+            options => [qw(
+                                pub
+                                author
+                                org
+                        )],
+                }
+        ],
+        input_boxes => 1,
+        input_ordered => 0,
+}, reuse => 1 );
+
+$c->add_dataset_field( "eprint", {
+   name => 'funders_other_text',
+   type => 'text',
+   multiple => 1,
+}, reuse => 1 );
+
+$c->add_dataset_field( "eprint", {
+   name => 'doi',
+   type => 'text',
+}, reuse => 1 );
--- lib/plugins/EPrints/Plugin/Screen/EPMC/DataPlus.pm
+++ lib/plugins/EPrints/Plugin/Screen/EPMC/DataPlus.pm
@@ -28,30 +28,59 @@ sub action_enable
 my $dataset_stage = '<?xml version="1.0"?>
 <workflow xmlns="http://eprints.org/ep3/workflow" xmlns:epc="http://eprints.org/ep3/control">
 	<stage name="dataset">
-		<component type="Upload" show_help="always"/>
-		<component type="Documents">
-			<field ref="content"/>
-			<field ref="format"/>
-			<field ref="formatdesc"/>
-			<field ref="security"/>
-			<field ref="license"/>
-			<field ref="date_embargo"/>
-			<!--  <field ref="relation" /> -->
-			<!--  <field ref="language" /> -->
-		</component>
+		<component><field ref="title" required="yes" input_lookup_url="{$config{rel_cgipath}}/users/lookup/title_duplicates" input_lookup_params="id={eprintid}&amp;dataset=eprint&amp;field=title" /></component>
+		<component><field ref="abstract" required="yes"/></component>
+		<component><field ref="keywords" required="yes"/></component>
+		<component><field ref="divisions" required="yes"/></component>
+
+		<component collapse="yes"><field ref="alt_title"/></component>
+		<component><field ref="creators" required="yes" input_lookup_url="{$config{rel_cgipath}}/users/lookup/name" /></component>
+		<component collapse="yes"><field ref="corp_creators"/></component>
+		
+		<epc:if test="$STAFF_ONLY = \'TRUE\'">
+			<component show_help="always"><field ref="doi" required="yes"/></component>
+		</epc:if>
+		<component><field ref="data_type" required="yes"/></component>
+
+		<component><field ref="contributors" collapse="yes" /></component>
 		<component type="Field::Multi">
-			<title>Dataset Information</title>
-			<field ref="title" required="yes" input_lookup_url="{$config{rel_cgipath}}/users/lookup/title_duplicates" input_lookup_params="id={eprintid}&amp;dataset=eprint&amp;field=title"/>
-			<field ref="data_type" required="yes"/>
-			<field ref="abstract"/>
-			<field ref="creators" input_lookup_url="{$config{rel_cgipath}}/users/lookup/name"/>
-			<field ref="contributors" />
-			<field ref="publisher" required="yes"/>
-			<field ref="date"/>
-			<field ref="official_url"/>
-			<field ref="bounding_box"/>
-			
+		    <title>Research Funders</title>
+		    <field ref="funders" required="yes" />
+		    <field ref="funders_other_text" />
 		</component>
+		<component><field ref="grant" collapse="yes" /></component>
+		<component collapse="yes"><field ref="projects"/></component>
+		<component type="Field::Multi">
+		    <title>Time period</title>
+		    <help>Help text here</help>
+		    <field ref="date_coll" required="yes" />
+		    <field ref="date_range" required="yes" />
+		</component>
+		<component collapse="yes"><field ref="geog"/></component>
+		<component type="Field::Multi" show_help="always" collapse="yes">
+		    <title>Geographic location</title>
+		    <help>Enter if applicable the Longitude and Latitude values of a theoretical geographic bounding rectangle that would cover the region in which your data were collected. You can use</help>
+		    <field ref="bounding_box" />
+		</component>
+		<component collapse="yes"><field ref="coll_samp"/></component>
+		<component collapse="yes"><field ref="legal_ethical"/></component>
+		<component collapse="yes"><field ref="lineage"/></component>
+		<component><field ref="language" required="yes"/></component>
+		<component collapse="yes"><field ref="note"/></component>
+		<component collaspe="yes"><field ref="related_resources"/></component>
+		<component type="Field::Multi">
+		    <title> Original Publication Details</title>
+		    <field ref="publisher"/>
+		    <field ref="ispublished" required="yes"/>
+		    <field ref="official_url"/>
+		    <field ref="date" />
+		    <field ref="date_type"/>
+		</component>
+		<component><field ref="copyright_holders" required="yes" /></component>
+
+
+		<component><field ref="contact_email" required="yes"/></component>
+		<component collapse="yes"><field ref="suggestions"/></component>
 	</stage>
 </workflow>';
 
@@ -77,7 +106,9 @@ my $dataset_stage = '<?xml version="1.0"?>
 
 	my $when_statement = $xml->create_element("epc:when", test=>"type = 'dataset'");
 	$choose_statement->appendChild($when_statement);
+	$when_statement->appendChild($xml->create_element("stage", ref=>"files"));
 	$when_statement->appendChild($xml->create_element("stage", ref=>"dataset"));
+	$when_statement->appendChild($xml->create_element("stage", ref=>"subjects"));
 
 	my $otherwise_statement = $xml->create_element("epc:otherwise");
 	$choose_statement->appendChild($otherwise_statement);
@@ -103,32 +134,43 @@ sub action_disable
 	my $filename = $repo->config( "config_path" )."/workflows/eprint/default.xml";
 
 	my $dom = $xml->parse_file( $filename );
-	my $choose = $dom->ownerDocument->getElementById("dataplus_choose");
-	my @choices = $dom->getElementsByTagName("epc:choose");
+
+	my $choose;
+	my @choices = $dom->getElementsByTagName("choose");
 	foreach my $element (@choices)
 	{
-		print STDERR $xml->to_string($element)."bacon\n\n";
 		if($element->hasAttribute("required_by") && $element->getAttribute("required_by") eq $self->{package_name})
 		{
 			$choose = $element;
 			last;
 		}
 	}
-	my $choose_parent = $choose->parentNode; #probably the flow element but err on the side of caution
-	my @otherwise = $choose->getElementsByTagName("otherwise");
 
-        foreach my $element ($otherwise[0]->childNodes()){
-                $element->unbindNode();
-                $choose_parent->appendChild($element);
-        }
+	if(defined $choose) {
+		my $choose_parent = $choose->parentNode; #probably the flow element but err on the side of caution
+		my @otherwise = $choose->getElementsByTagName("otherwise");
+		foreach my $element ($otherwise[0]->childNodes()){
+			$element->unbindNode();
+			$choose_parent->appendChild($element);
+		}
+
+		open( FILE, ">", $filename );
+
+		print FILE $xml->to_string($dom, indent=>1);
+
+		close( FILE );
+	}
+
+	EPrints::XML::remove_package_from_xml( $filename, $self->{package_name} );
+
+
+	$dom = $xml->parse_file( $filename );
 
 	open( FILE, ">", $filename );
 
 	print FILE $xml->to_string($dom, indent=>1);
 
 	close( FILE );
-
-	EPrints::XML::remove_package_from_xml( $filename, $self->{package_name} );
 
 	$self->reload_config if !$skip_reload;
 }
